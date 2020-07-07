5c09021661f2d6e13793575af21ae9892e7e6f96
jbt <jbt@302_INS>
2020-07-07 21:43:44 +0300

    aaa

---
+++ HelloServlet/src/a/HelloServlet.java
@@ -0,0 +1,21 @@
+package a;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// write a response to the client (browser)
+		PrintWriter out = resp.getWriter();
+		out.println("Hello From Servelt: " + Math.random());
+	}
+}
