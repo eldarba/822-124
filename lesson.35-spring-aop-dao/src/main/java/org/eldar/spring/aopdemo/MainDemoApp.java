525aff169a0483122c1c2245f050b06430fd4338
jbt <jbt@302_INS>
2020-07-31 13:14:39 +0300

    aaa

--- lesson.35-spring-aop-dao/src/main/java/org/eldar/spring/aopdemo/MainDemoApp.java
+++ lesson.35-spring-aop-dao/src/main/java/org/eldar/spring/aopdemo/MainDemoApp.java
@@ -1,10 +1,23 @@
 package org.eldar.spring.aopdemo;
 
+import org.eldar.spring.AppConfig;
+import org.eldar.spring.dao.beans.CompanyDao;
+import org.eldar.spring.dao.beans.CouponDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MainDemoApp {
 
 	public static void main(String[] args) {
-		
-		
+
+		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class)) {
+			CompanyDao companyDao = ctx.getBean(CompanyDao.class);
+			CouponDao couponDao = ctx.getBean(CouponDao.class);
+
+			companyDao.addCompany();
+			companyDao.sayHello();
+			couponDao.addCoupon();
+			couponDao.doWork();
+		}
 
 	}
 
