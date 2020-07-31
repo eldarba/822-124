aa2fa54fdc5207bfff75999e522487e85e6f41e2
jbt <jbt@302_INS>
2020-07-31 12:35:28 +0300

    aaa

---
+++ lesson.35-spring-aop-dao/src/main/java/org/eldar/spring/AppConfig.java
@@ -0,0 +1,10 @@
+package org.eldar.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+}
---
+++ lesson.35-spring-aop-dao/src/main/java/org/eldar/spring/aopdemo/MainDemoApp.java
@@ -0,0 +1,11 @@
+package org.eldar.spring.aopdemo;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
