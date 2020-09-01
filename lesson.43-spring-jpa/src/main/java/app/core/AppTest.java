129cd057174f3e5e33a9896b31a9bded59f5733d
jbt <jbt@302_INS>
2020-09-01 19:44:02 +0300

    ddd

--- lesson.43-spring-jpa/src/main/java/app/core/AppTest.java
+++ lesson.43-spring-jpa/src/main/java/app/core/AppTest.java
@@ -17,10 +17,48 @@ public class AppTest {
 				Student st1 = new Student("aaa", "aaaMail", Gender.M);
 				Student st2 = new Student("bbb", "bbbMail", Gender.F);
 				Student st3 = new Student("ccc", "cccMail", Gender.F);
-				studentDao.addStudent(st1);
+				int id = studentDao.addStudent(st1);
 				studentDao.addStudent(st2);
 				studentDao.addStudent(st3);
+
+				System.out.println("id: " + id);
 			}
+
+//			{ // get student
+//				int id1 = 3;
+//				int id2 = 35;
+//				Student st1 = studentDao.getStudent(id1);
+//				Student st2 = studentDao.getStudent(id2);
+//				System.out.println(st1);
+//				System.out.println(st2);
+//			}
+
+//			{ // get all students
+//				System.out.println("----------------");
+//				List<Student> students = studentDao.getAllStudents();
+//				for (Student student : students) {
+//					System.out.println(student);
+//				}
+//				System.out.println("----------------");
+//			}
+
+//			{ // update student
+//				int id1 = 9;
+//				int id2 = 35;
+//				Student st1 = studentDao.getStudent(id1);
+//				Student st2 = new Student("aaaa", "abs@Mail", Gender.M);
+//				st2.setId(id2);
+//
+//				boolean updated;
+//				updated = studentDao.updateStudent(st1);
+//				System.out.println(st1 + " updated: " + updated);
+//				updated = studentDao.updateStudent(st2);
+//				System.out.println(st2 + " updated: " + updated);
+//			}
+
+//			{
+//				studentDao.deleteStudent(5);
+//			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
