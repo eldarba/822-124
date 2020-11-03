7fcfef70efc9b14dbf56d040326e6c2f3a302363
Eldar <eldarba@gmail.com>
2020-11-03 21:18:40 +0200

    aaa

--- lesson.57-rest-session-filters/src/main/java/app/core/controllers/MyController.java
+++ lesson.57-rest-session-filters/src/main/java/app/core/controllers/MyController.java
@@ -3,23 +3,45 @@ package app.core.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import app.core.models.Person;
+import app.core.sessions.Session;
+import app.core.sessions.SessionContext;
 
 @RestController
 @RequestMapping("/api")
 public class MyController {
 
 	private Map<Integer, Person> personsMap = new HashMap<>();
+	@Autowired
+	SessionContext sessionContext;
+
+	@PostMapping("/login/{userName}")
+	public String login(@PathVariable String userName) {
+		Session session = sessionContext.createSession();
+		session.setAttribute("userName", userName);
+		return session.token;
+	}
+
+	@GetMapping("/greet2")
+	public String greet2(@RequestHeader String token) {
+		Session session = sessionContext.getSession(token);
+		if (session != null) {
+			return "Hello " + session.getAttribute("userName");
+		}
+		return "Hello unlogged User";
+	}
 
 	@GetMapping("/greet")
 	public String greet() {
--- lesson.57-rest-session-filters/src/main/java/app/core/sessions/SessionContext.java
+++ lesson.57-rest-session-filters/src/main/java/app/core/sessions/SessionContext.java
@@ -2,7 +2,12 @@ package app.core.sessions;
 
 import java.util.Map;
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,6 +26,59 @@ public class SessionContext {
 	// methods
 	private boolean isSessionExpired(Session session) {
 		return System.currentTimeMillis() - session.getLastAccessed() > session.getMaxInactiveInterval();
+	}
+
+	@PostConstruct
+	private void init() {
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				System.out.println("removing expired sessions");
+
+				for (String token : sessionsMap.keySet()) {
+					Session session = sessionsMap.get(token);
+					if (isSessionExpired(session)) {
+						invalidate(session);
+					}
+				}
+
+			}
+		};
+
+		timer.schedule(task, 3000, TimeUnit.SECONDS.toMillis(removeExpiredPeriod));
+	}
+
+	// stop the thread
+	@PreDestroy
+	private void destroy() {
+		timer.cancel();
+		System.out.println("removing expired coupons thread stopped");
+	}
+
+	public Session createSession() {
+		Session session = ctx.getBean(Session.class);
+		sessionsMap.put(session.token, session);
+		return session;
+	}
+
+	public void invalidate(Session session) {
+		sessionsMap.remove(session.token);
+	}
+
+	public Session getSession(String token) {
+		Session session = sessionsMap.get(token);
+		if (session != null) {
+			if (!isSessionExpired(session)) {
+				session.resetLastAccess();
+				return session;
+			} else {
+				invalidate(session);
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
 }
